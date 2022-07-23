1471ce5e8be4913addc02da7708608da104b8c55
Ricardo Borutta <devcyntrix@hotmail.com>
2022-07-23 15:40:06 +0200

    Simplify code

--- WatchedBlockAir/src/main/java/com/tizfaver/watchedblockbecomesair/WatchEvent.java
+++ WatchedBlockAir/src/main/java/com/tizfaver/watchedblockbecomesair/WatchEvent.java
@@ -11,11 +11,11 @@ import org.bukkit.event.player.PlayerMoveEvent;
 public class WatchEvent implements Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent e) {
-        if(WatchedBlockBecomesAir.status == true) {
-            Player player = e.getPlayer();
-            Block block = player.getTargetBlock(null, 10);
-            Location loc = block.getLocation();
-            player.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).setType(Material.AIR);
-        } else { }
+        if (!WatchedBlockBecomesAir.status)
+            return;
+        Player player = e.getPlayer();
+        Block block = player.getTargetBlock(null, 10);
+        Location loc = block.getLocation();
+        player.getWorld().getBlockAt(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ()).setType(Material.AIR);
     }
 }
